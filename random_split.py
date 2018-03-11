81703241e2dfaaa868ea0a759ab477eb88da4579
czqInNanjing <czqofnju@gmail.com>
2018-03-11 22:00:01 +0800

    add split code

--- random_split.py
+++ random_split.py
@@ -1,5 +1,8 @@
 import utils.config
+import os
+import os.path as osp
 import pickle
+import glob
 
 def random_split():
     cfg = utils.config.config()
@@ -7,11 +10,27 @@ def random_split():
     test = []
     train_data = []
     val_data = []
-    train = get_file_list('train', cfg.data_root, cfg.data_views)
+    train = get_filename_list('train', cfg.data_root, cfg.data_views)
 
-
-def get_file_list(data_type, root, data_views):
+def get_d_list(d_root, data_views):
     pass
+
+
+def get_filename_list(data_type, root, data_views):
+    filename_list = []
+    data_all = glob.glob(osp.join(root, '*'))
+    data_all = [data for data in data_all if osp.isdir(data)]
+    for _idx, d in enumerate(data_all):
+        d_lbl = osp.split(d)[1]
+        d_lbl_idx = _idx
+        d_root = osp.join(root, data_type)
+        d_list = get_d_list(d_root, data_views)
+        filename_list.append({'label': d_lbl,
+                              'label_idx': d_lbl_idx,
+                              'imgs': d_list})
+    return filename_list
+
+    print(data_all)
 
 
 if __name__ == '__main__':
