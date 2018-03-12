02e64403435163c60b5272597d3f38d62e2aeaf7
czqInNanjing <czqofnju@gmail.com>
2018-03-12 21:26:27 +0800

    add split code

--- random_split.py
+++ random_split.py
@@ -1,5 +1,6 @@
 import utils.config
 import os
+import sys
 import os.path as osp
 import pickle
 import glob
@@ -11,26 +12,59 @@ def random_split():
     train_data = []
     val_data = []
     train = get_filename_list('train', cfg.data_root, cfg.data_views)
+    test = get_filename_list('test', cfg.data_root, cfg.data_views)
+
 
 def get_d_list(d_root, data_views):
-    pass
+    """
+    get all structed filenames in one class
+    :param d_root:
+    :param data_views:
+    :return:
+    -bench_0001--
+               |-/home/fyf/code/mvcnn/data/12_ModelNet40/bench/train/bench_0001_001.jpg
+               |-/home/fyf/code/mvcnn/data/12_ModelNet40/bench/train/bench_0001_002.jpg
+               |-......
+    -bench_0002--
+               |-......
+    """
+    full_names = glob.glob(osp.join(d_root, '*.jpg'))
+    structed_data = []
+    raw_structed_data = {}
+    names = [osp.split(name)[1] for name in full_names]
+    for _idx, name in enumerate(names):
+        shape_name = name[:name.rfind('_')]
+        if shape_name not in raw_structed_data:
+            raw_structed_data[shape_name] = [full_names[_idx]]
+        else:
+            raw_structed_data[shape_name].append(full_names[_idx])
+
+    raw_views = len(raw_structed_data.items()[0][1])
+    view_seq = raw_views//data_views
+    assert view_seq*data_views == raw_views, 'wrong data views, view seq can not be int'
+
+    # for k, v in raw_structed_data.items():
+    #     structed_data.append({k: v[::view_seq]})
+    structed_data = [{k: v[::view_seq]} for k, v in raw_structed_data.items()]
+
+    return structed_data
 
 
 def get_filename_list(data_type, root, data_views):
     filename_list = []
     data_all = glob.glob(osp.join(root, '*'))
+    data_all = sorted(data_all)
     data_all = [data for data in data_all if osp.isdir(data)]
     for _idx, d in enumerate(data_all):
         d_lbl = osp.split(d)[1]
         d_lbl_idx = _idx
-        d_root = osp.join(root, data_type)
+        d_root = osp.join(root, d_lbl, data_type)
         d_list = get_d_list(d_root, data_views)
         filename_list.append({'label': d_lbl,
                               'label_idx': d_lbl_idx,
                               'imgs': d_list})
     return filename_list
 
-    print(data_all)
 
 
 if __name__ == '__main__':
